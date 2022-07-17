b1260d445de463cd16d43a1bb41a956089a3fa25
Yevhen Prokhorenko <prokhorenko.yevhen@gmail.com>
2022-07-17 14:46:09 +0300

    second task completed

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,7 +5,7 @@ const galleryWrapper = document.querySelector(".gallery");
 
 function createLi(galleryItems) {
 
-    return galleryItems.reduce((acc, img) => acc + `<div class="gallery__item"><a class="gallery__link" href="${img.original}"><img class="gallery__image" src="${img.preview}" data-source="${img.original}" alt="${img.descreption}"/></a> </div>`, "");
+    return galleryItems.reduce((acc, img) => acc + `<div class="gallery__item"><a class="gallery__link" href="${img.original}"><img class="gallery__image" src="${img.preview}" data-source="${img.original}" alt="${img.description}"/></a> </div>`, "");
 }
 
 const result = createLi(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryWrapper = document.querySelector(".gallery");
 
+
+function createLi(galleryItems) {
+
+    return galleryItems.reduce((acc, img) => acc +
+        `<a class="gallery__item" href="${img.original}"><img class="gallery__image" src="${img.preview}" alt="${img.description}"/></a>`, "");
+}
+
+const result = createLi(galleryItems);
+galleryWrapper.insertAdjacentHTML("beforeend", result);
+galleryWrapper.addEventListener('click', openModal);
+
+
+function openModal(e) {
+    e.preventDefault();
+    const lightbox = new SimpleLightbox('.gallery a', {
+        captionsData: "alt",
+        captionDelay: 250,
+        captionPosition: "bottom",
+    });
+    lightbox.open();    
+}
 console.log(galleryItems);
