4835079eaacac46525ded8c247e61161e94a3c71
Yevhen Prokhorenko <prokhorenko.yevhen@gmail.com>
2022-07-19 19:57:30 +0300

    fix first task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -12,18 +12,38 @@ const result = createLi(galleryItems);
 galleryWrapper.insertAdjacentHTML("beforeend", result);
 
 
-function openModal(e) {
-    e.preventDefault();
-    const instance = basicLightbox.create(`<img src="${e.target.dataset.source}" width="800" height="600"/>`);
-    window.addEventListener(`keydown`, onKeyPress);
-    function onKeyPress(e) {
-    if (e.key === `Escape`) {        
-        instance.close()
-    }   
-}
+// function openModal(e) {
+//     e.preventDefault();
+//     const instance = basicLightbox.create(`<img src="${e.target.dataset.source}" width="800" height="600"/>`);
+//     window.addEventListener(`keydown`, onKeyPress);
+//     function onKeyPress(e) {
+//     if (e.key === `Escape`) {        
+//         instance.close()
+//     }   
+// }
 
-    instance.show();       
-}
+//     instance.show();       
+// }
+
 galleryWrapper.addEventListener('click', openModal);
 
-console.log(galleryItems);
+const instance = basicLightbox.create(`<img src="" width="800" height="600"/>`, {
+        onShow: instance => {
+            window.addEventListener(`keydown`, EscapeCloseImg);
+        },
+        onClose: instance => {
+            window.removeEventListener(`keydown`, EscapeCloseImg);
+        },
+});
+function openModal(e) {
+    e.preventDefault();
+    instance.element().querySelector('img').src = e.target.dataset.source;
+    instance.show();
+}
+
+function EscapeCloseImg(e) {
+    if (e.key === `Escape`) {
+        instance.close();
+        return; 
+    }  
+}
